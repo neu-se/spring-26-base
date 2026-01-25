b3bcda15930e675f322072993ceeb8e961d4ef1f
Rob Simmons <robsimmons@gmail.com>
2026-01-25 11:15:18 -0500

    Prettier reformat

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 /* eslint no-console: "off" */
 
-import { app } from './app.ts';
+import { app } from "./app.ts";
 
-const PORT = parseInt(process.env.PORT || '3000');
+const PORT = parseInt(process.env.PORT || "3000");
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
